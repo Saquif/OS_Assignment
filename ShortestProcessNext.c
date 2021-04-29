9f602e8fc4b91158ea9e313dc74d90bffbceb356
Tanvir Anjum Labir <33982784+Labir852@users.noreply.github.com>
2021-04-29 12:45:03 +0600

    Add files via upload

---
+++ ShortestProcessNext.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+int main()
+{
+    int time_quantum,process_num,i,j;
+    float alpha,ready_Q[100],blocked_Q[100];
+
+    printf("Enter the value of alpha: ");
+    scanf("%d",&alpha);
+
+    printf("\nEnter the value of time_quantum: ");
+    scanf("%d",&time_quantum);
+
+    printf("\nEnter how many processes running: ");
+    scanf("%d",&process_num);
+
+    float processes[process_num];
+    int cpu_burst[process_num],arrival_time[process_num];
+
+    printf("\nEnter The CPU BURST respectively: ");
+    for(i=0;i<process_num;i++)
+    {
+        scanf("%d",&cpu_Burst[i]);
+    }
+    printf("\nEnter the arrival time respectively: ");
+    for(i=0;i<process_num;i++)
+    {
+        scanf("%d",&arrival_time[i]);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
